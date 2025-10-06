70f50446dd5f71770497623942909292d4734fe9
Vinicius Souza <vini_souza1992@hotmail.com>
2025-10-06 16:50:48 -0300

    subindo tudo

--- VelhosDitados.API/obj/Debug/net8.0/VelhosDitados.API.AssemblyInfo.cs
+++ VelhosDitados.API/obj/Debug/net8.0/VelhosDitados.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VelhosDitados.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec17f1201a7daedb2f40325bcc1546e95885b2e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VelhosDitados.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VelhosDitados.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
